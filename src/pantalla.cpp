af9488a3c2f798bf2baf3802b6b85e3f8083fcf7
Jose_Carlos <mora.moreno.josecarlos@gmail.com>
2025-07-11 09:35:53 -0600

    Version Proceso 1.1.3.2: Funcion de pantalla encendida y apagada

--- src/pantalla.cpp
+++ src/pantalla.cpp
@@ -1,28 +1,20 @@
 #include <Arduino.h>
 #include "pantalla.h"
-#include "heltec.h"
 
 #define ENTRADA_FIJA 1  
 
-String Version = "1.1.1.1";
-
-
+String Version = "1.1.3.2";
 bool Pantalla = false;
 
 void Pantallas(bool estado) {
-    Pantalla = estado;
+Pantalla = estado;
 
-    pinMode(ENTRADA_FIJA, OUTPUT);
-    digitalWrite(ENTRADA_FIJA, Pantalla ? HIGH : LOW);
+pinMode(ENTRADA_FIJA, OUTPUT);
+digitalWrite(ENTRADA_FIJA, Pantalla ? HIGH : LOW);
 
-    if (!Pantalla) {
-    Heltec.display->clear();
-    Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
-    Heltec.display->setFont(ArialMT_Plain_10);
-    Heltec.display->drawString(64, 30, "Pantalla apagada");
-    Heltec.display->display();
-    Serial.println("Pantalla OLED: APAGADA");
-    } else {
-    Serial.println("Pantalla OLED: ENCENDIDA");
-    }
+if (!Pantalla) {
+Serial.println("Pantalla OLED: APAGADA");
+} else {
+Serial.println("Pantalla OLED: ENCENDIDA");
+}
 }
